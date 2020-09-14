0d9da4e7a61d725d98dc969358dbe926d04576f2
calaf <calafiore.e.a@gmail.com>
2020-09-14 12:10:06 +0200

    p

---
+++ app/src/main/java/com/e/todolist/MainActivity.java
@@ -0,0 +1,117 @@
+package com.e.todolist;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    final List<String> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listView= findViewById(R.id.listView);
+        final TextAdaptor adapter = new TextAdaptor();
+
+        adapter.setData(list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Delete this task?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                list.remove(position);
+                                adapter.setData(list);
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .create();
+                dialog.show();
+            }
+        });
+
+        final Button newTaskButton = findViewById(R.id.newTaskButton);
+
+        newTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText taskInput = new EditText(MainActivity.this);
+                taskInput.setSingleLine();
+                AlertDialog dialog= new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Add a new task")
+                        .setMessage("What is your new task")
+                        .setView(taskInput)
+                        .setPositiveButton("Add task", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                list.add(taskInput.getText().toString());
+                                adapter.setData(list);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                 dialog.show();
+            }
+        });
+    }
+
+
+     class TextAdaptor extends BaseAdapter{
+
+        List<String> list = new ArrayList<>();
+
+        void setData(List<String> mlist){
+            list.clear();
+            list.addAll(mlist);
+            notifyDataSetChanged();
+        }
+
+
+        @Override
+        public int getCount(){
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position){
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position){
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater infLater = (LayoutInflater)
+                         MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView= infLater.inflate(R.layout.item, parent, false);
+            TextView textView= rowView.findViewById(R.id.task);
+            textView.setText(list.get(position));
+            return rowView;
+        }
+    }
+}
